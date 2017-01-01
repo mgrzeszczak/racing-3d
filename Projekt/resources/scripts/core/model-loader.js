48d9a5e5f3e485981900ba7d85a2428a153909f5
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2017-01-01 19:19:31 +0100

    Textures.

--- Projekt/resources/scripts/core/model-loader.js
+++ Projekt/resources/scripts/core/model-loader.js
@@ -56,7 +56,7 @@ app.modelLoader = (function(){
 
         var texture = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
